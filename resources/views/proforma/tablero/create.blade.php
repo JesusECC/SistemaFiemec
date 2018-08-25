9a614aa44898a4f17d01d0881075fa4a009d3701
Jesus <jesuschilca@gmail.com>
2018-08-25 13:36:47 -0500

    tableros con +

--- resources/views/proforma/tablero/create.blade.php
+++ resources/views/proforma/tablero/create.blade.php
@@ -13,6 +13,15 @@
 		</ul>	
     </div>
     @endif
+    <label class="control-label">Ingresar Nombre de tablero</label>
+    <div class="form-group">
+        <div class="input-group">
+            <input type="text" class="form-control" name="NomTablerop" id="NomTablerop" placeholder="Ingresar nombre del tablero..." >
+            <samp class="input-group-btn">
+                <button type="button" id="bt_add_tablero" class="btn btn-primary">Agregar</button>
+            </samp>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-sm-7">
@@ -20,7 +29,7 @@
                 <label class="control-label">Producto</label>
                 <select name="pidProducto" class="form-control selectpicker" id="pidProducto" data-live-search="true">
                     @foreach($productos as $producto)
-                        <option value="{{ $producto->idProducto }}">{{ $producto->nombre_producto }}</option>
+                        <option value="{{ $producto->idProducto }}_{{ $producto->nombre_producto }}_{{ $producto->precio_unitario }}">{{ $producto->nombre_producto }}</option>
                     @endforeach
                 </select>
             </div>
@@ -39,6 +48,7 @@
                 <div class="col-sm-2">
                     <div class="form-group label-floating">
                         <label class="control-label">Producto</label>
+                        <input type="hidden" id="idProd" name="idProd" disabled>
                         <input type="text" id="Productoname" class="form-control" name="Productoname" disabled>
                     </div>
                 </div>
@@ -46,14 +56,14 @@
                 <div class="col-sm-1">
                     <div class="form-group label-floating">
                         <label class="control-label">P. UNIT.</label>
-                        <input type="number"  id="precio_uni" class="form-control" name="precio_uni" value="{{ old('price') }}" disabled>
+                        <input type="number"  id="precio_uni" class="form-control" name="precio_uni"  disabled>
                     </div>
                 </div>
 
                 <div class="col-sm-1">
                     <div class="form-group label-floating">
                         <label class="control-label">Cantidad</label>
-                        <input type="number" id="Pcantidad" class="form-control" name="Pcantidad" value="{{ old('price') }}" >
+                        <input type="number" id="Pcantidad" class="form-control" name="Pcantidad" >
                     </div>
                 </div>
 
@@ -67,7 +77,7 @@
                 <div class="col-sm-1">
                     <div class="form-group label-floating">
                     <label class="control-label"></label>
-                        <button type="button" id="bt_add" class="btn btn-primary">Agregar</button>
+                        <button type="button" id="bt_add_produc" class="btn btn-primary">Agregar</button>
                     </div>
                 </div>
             </div>
@@ -100,68 +110,130 @@
 @push('scripts')
 <script>
 $(document).ready(function(){
-    $('#bt_add').click(function(){
+    $('#bt_add_tablero').click(function(){
         agregarTablero();
+    });
+    $('#bt_add_produc').click(function(){
+        agregarProductosTablero();
+    });
+    $('#bt_add_produc').click(function(){
+        agregarProductosTablero();
     });
 });
 var tablero=[];
+var filaob=[];
 var cont=0;
+var contp=0;
 var table;
-var nomTablero;
-function agregarTablero(){
+var subtotal=0;
+//var nomTablero;
+$("#pidProducto").change(MostarProducto);
+$("#bt_add_tablero").change($("#total_"+nomTablero).html("s/. " + subtotal));
+
+function MostarProducto(){
+    Producto=document.getElementById('pidProducto').value.split('_');
+    $("#idProd").val(Producto[0]);
+    $("#Productoname").val(Producto[1]);
+    $("#precio_uni").val(Producto[2]);
+}
+
+function agregarProductosTablero(){    
+    var idProd=$('#idProd').val();
+    var pname=$("#Productoname ").val();
+    var puni=$('#precio_uni').val();
+    var pcant=$('#Pcantidad').val();
+    var nomTablero=$("#NomTablero").val();
+    var filas;
     
-    nomTablero=$("#NomTablero").val();
-    console.log(tablero.includes(nomTablero));
-    var bool=Boolean(tablero.includes(nomTablero));
+    if(tablero.length>=0 && nomTablero!="" && idProd!="" && pname!="" && puni!="" && pcant!="" && nomTablero!=""){
+        var bool=Boolean(tablero.includes(nomTablero));
+        contp++;
+        if(bool==true ){
+            subtotal+=pcant*puni;
+                    filas=
+                            '<tr class="selected" id="fila'+contp+'">'+
+                                '<td> '+ 
+                                    '<input type="hidden" name="idpod_'+nomTablero+'[]" value="'+idProd+'">'+pname+
+                                '</td>'+
+                                '<td> '+ 
+                                    '<input type="number" disabled name="pcant'+nomTablero+'[]" value="'+pcant+'">'+
+                                '</td>'+
+                                '<td> '+   
+                                    '<input type="number" disabled name="preuni'+nomTablero+'[]" value="'+puni+'">'+
+                                '</td>'+
+                                '<td> '+   
+                                    '<input type="number" disabled name="ptotal'+nomTablero+'[]" value="'+subtotal+'">'+
+                                '</td>'+
+                            '</tr>';
+                        $('#detalle_'+nomTablero).append(filas);
+                        // $("#total_"+nomTablero).html("s/. " + subtotal);
+        }
+        var dat={nombre:nomTablero,fila:filas,subtota:pcant*puni};
+        filaob.push(dat);
+
+    }
+    nomtablero="";
+}
+function agregarTablero(){    
+    var nomTablero=$("#NomTablerop").val();
+    //console.log(idProd,pname,puni,pcant);
+    
     if(tablero.length>=0 && nomTablero!=""){
+        var bool=Boolean(tablero.includes(nomTablero));
         tablero[cont]=nomTablero;
         cont++;
         if(bool!=true ){
             table+='<div class="panel panel-default">'+
                         '<div class="panel-heading">'+
-                            '<h3 class="panel-title">'+nomTablero+'</h3>'+
+                            '<h3 class="panel-title">Tablero '+nomTablero+'</h3>'+
                         '</div>'+
                         '<div class="panel-body">'+
-                            '<table id="detalles" class="table table-striped table-bordered table-condensed table-hover">'+
+                            '<table id="detalle_'+nomTablero+'" class="table table-striped table-bordered table-condensed table-hover">'+
                                 '<thead style="background-color:#A9D0F5">'+
-                                    '<th>Item</th>'+
-                                    '<th>Descripcion</th>'+
-                                    '<th>Uni.</th>'+
+                                    '<th>Producto</th>'+
                                     '<th>Cant.</th>'+
                                     '<th>P. Unit.</th>'+
                                     '<th>Importe</th>'+
                                 '</thead>'+
-                                '<tfoot>'+
-                                    '<th>total</th>'+
-                                    '<th></th>'+
-                                    '<th></th>'+
-                                    '<th></th>'+
-                                    '<th></th>'+
-                                    '<th><h4 id="total">s/. 0.00</h4><input type="hidden" name="precio_total" id="precio_total">'+
-                                    '</th>'+
-                                '</tfoot>'+
                                 '<tbody>'+
-                                '</tbody>'+            		
+                                '</tbody>'+ 
+                                // '<tfoot>'+
+                                //     '<th>Total</th>'+
+                                //     '<th></th>'+
+                                //     '<th></th>'+
+                                //     '<th><h4 id="total_'+nomTablero+'">s/. 0.00</h4><input type="hidden" name="precio_subtotal_'+nomTablero+'" id="precio_subtotal_'+nomTablero+'">'+
+                                //     '</th>'+
+                                // '</tfoot>'+
                             '</table>'+
                         '</div>'+
-                    '</div>';
-        }else{
-            console.log("anademe");
+                    '</div>'+
+                    '<table id="detalle_'+nomTablero+'" class="table table-striped table-bordered table-condensed table-hover">'+
+                        '</tbody>'+ 
+                            '<tfoot>'+
+                                '<th colspan="3" >Total</th>'+
+                                '<th><h4 id="total">s/. 0.00</h4><input type="hidden" name="precio_subtotal" id="precio_subtotal"></th>'+
+                            '</tfoot>'+
+                        '</table>'+
+                    '</table>';
+        }        
+    }
+    
+    $('#tablerosn').html(table);
+    if(tablero.length>0){
+        for (var i = 0; i < tablero.length; i++) {
+            for (const key in filaob) {
+                if (filaob.hasOwnProperty(key)) {
+                    if(tablero[i]==filaob[key]['nombre'])
+                    $('#detalle_'+tablero[i]).append(filaob[key]['fila']);
+                }
+            }
         }
     }
     nomtablero="";
-    for (var i = 0; i < tablero.length; i++) {
-        console.log(tablero[i]);
-    }
-    $('#tablerosn').html(table);
-    // if(tablero.length >0){
-    //     tablero.push(nomTablero);
-    // }
-
 }
-    
+
+
+
 </script>
-
 @endpush
-
 @endsection
