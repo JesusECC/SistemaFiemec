f77dcffca57fa3e97c1e38227084b8d2c9a208f4
Jesus <jesuschilca@gmail.com>
2018-08-26 17:16:35 -0500

    eliminar

--- resources/views/proforma/tablero/create.blade.php
+++ resources/views/proforma/tablero/create.blade.php
@@ -84,12 +84,8 @@
         </div>
     </div>
     <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-
-
         <div id="tablerosn" >
-        </div>          
-
-    
+        </div>   
     <!-- JOSE CORRIGE EL MARGIN DEL BOTON VOLVER CTMR!!!! -->
     <div style="margin-top: 20px" class="from-group ">
 
@@ -109,133 +105,160 @@
 
 @push('scripts')
 <script>
-$(document).ready(function(){
-    $('#bt_add_tablero').click(function(){
-        agregarTablero();
+    $(document).ready(function(){
+        $('#bt_add_tablero').click(function(){
+            agregarTablero();
+        });
+        $('#bt_add_produc').click(function(){
+            agregarProductosTablero();
+        });
     });
-    $('#bt_add_produc').click(function(){
-        agregarProductosTablero();
-    });
-});
-var tablero=[];
-var filaob=[];
-var cont=0;
-var contp=0;
-var table;
-var subtotal=0;
-//var nomTablero;
-$("#pidProducto").change(MostarProducto);
-//$("#bt_add_tablero").change($("#total").html("s/. " + subtotal));
+    var tablero=[];
+    var filaob=[];
+    var cont=0;
+    var contp=0;
+    var table;
+    var subtotal=0;
+    //var nomTablero;
+    $("#pidProducto").change(MostarProducto);
+    //$("#bt_add_tablero").change($("#total").html("s/. " + subtotal));
 
-function MostarProducto(){
-    Producto=document.getElementById('pidProducto').value.split('_');
-    $("#idProd").val(Producto[0]);
-    $("#Productoname").val(Producto[1]);
-    $("#precio_uni").val(Producto[2]);
-}
+    function MostarProducto(){
+        Producto=document.getElementById('pidProducto').value.split('_');
+        $("#idProd").val(Producto[0]);
+        $("#Productoname").val(Producto[1]);
+        $("#precio_uni").val(Producto[2]);
+    }
 
-function agregarProductosTablero(){    
-    var idProd=$('#idProd').val();
-    var pname=$("#Productoname ").val();
-    var puni=$('#precio_uni').val();
-    var pcant=$('#Pcantidad').val();
-    var nomTablero=$("#NomTablero").val();
-    var filas;
-    
-    if(tablero.length>=0 && nomTablero!="" && idProd!="" && pname!="" && puni!="" && pcant!="" && nomTablero!=""){
-        var bool=Boolean(tablero.includes(nomTablero));
-        contp++;
-        if(bool==true ){
-            subtotal+=pcant*puni;
-                    filas=
-                            '<tr class="selected" id="fila'+contp+'">'+
-                                '<td> '+ 
-                                    '<input type="hidden" name="idpod_'+nomTablero+'[]" value="'+idProd+'">'+pname+
-                                '</td>'+
-                                '<td> '+ 
-                                    '<input type="number" disabled name="pcant'+nomTablero+'[]" value="'+pcant+'">'+
-                                '</td>'+
-                                '<td> '+   
-                                    '<input type="number" disabled name="preuni'+nomTablero+'[]" value="'+puni+'">'+
-                                '</td>'+
-                                '<td> '+   
-                                    '<input type="number" disabled name="ptotal'+nomTablero+'[]" value="'+subtotal+'">'+
-                                '</td>'+
-                            '</tr>';
-                        $('#detalle_'+nomTablero).append(filas);
-                        // $("#total_"+nomTablero).html("s/. " + subtotal);
+    function agregarProductosTablero(){    
+        var idProd=$('#idProd').val();
+        var pname=$("#Productoname ").val();
+        var puni=$('#precio_uni').val();
+        var pcant=$('#Pcantidad').val();
+        var nomTablero=$("#NomTablero").val();
+        var filas;
+        
+        if(tablero.length>=0 && nomTablero!="" && idProd!="" && pname!="" && puni!="" && pcant!="" && nomTablero!=""){
+            var bool=Boolean(tablero.includes(nomTablero));
+            contp++;
+            if(bool==true ){
+                subtotal+=pcant*puni;
+                        filas=
+                                '<tr class="selected" id="fila_'+nomTablero+'_'+contp+'">'+
+                                    '<td> '+ 
+                                        '<input type="hidden" name="idpod_'+nomTablero+'[]" value="'+idProd+'">'+pname+
+                                    '</td>'+
+                                    '<td> '+ 
+                                        '<input type="number" disabled name="pcant'+nomTablero+'[]" value="'+pcant+'">'+
+                                    '</td>'+
+                                    '<td> '+   
+                                        '<input type="number" disabled name="preuni'+nomTablero+'[]" value="'+puni+'">'+
+                                    '</td>'+
+                                    '<td> '+   
+                                        '<input type="number" disabled name="ptotal'+nomTablero+'[]" value="'+pcant*puni+'">'+
+                                    '</td>'+
+                                    '<td>'+
+                                        '<button type="button" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs" onclick="eliminar('+contp+');">'+
+                                                '<i class="fa fa-times"></i>'+
+                                        '</button>'+
+                                    '</td>'+
+                                '</tr>';
+                            $('#detalle_'+nomTablero).append(filas);
+                            // $("#total_"+nomTablero).html("s/. " + subtotal);
+            }        
+            //console.log(filas);
+            var dat={nombre:nomTablero,fila:filas,subtotal:pcant*puni,posi:contp};
+            filaob.push(dat);
+            //console.log(filaob,contp)
+
         }
-        console.log(filas);
-        var dat={nombre:nomTablero,fila:filas,subtota:pcant*puni};
-        filaob.push(dat);
-
+        nomtablero="";
     }
-    nomtablero="";
-}
-function agregarTablero(){    
-    var nomTablero=$("#NomTablerop").val();
-    //console.log(idProd,pname,puni,pcant);
-    
-    if(tablero.length>=0 && nomTablero!=""){
-        var bool=Boolean(tablero.includes(nomTablero));
-        tablero[cont]=nomTablero;
-        cont++;
-        if(bool!=true ){
-            table+='<div class="panel panel-default">'+
-                        '<div class="panel-heading">'+
-                            '<h3 class="panel-title">Tablero '+nomTablero+'</h3>'+
+    function agregarTablero(){    
+        var nomTablero=$("#NomTablerop").val();
+        //console.log(nomTablero);
+        
+        if(tablero.length>=0 && nomTablero!=""){
+            var bool=Boolean(tablero.includes(nomTablero));
+            
+            console.log(bool);
+            if(bool!=true ){
+                tablero[cont]=nomTablero;
+                table+='<div class="panel panel-default">'+
+                            '<div class="panel-heading">'+
+                                '<h3 class="panel-title">Tablero '+nomTablero+'</h3>'+
+                                '<button type="button" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs" onclick="eliminarTablero('+cont+');">'+
+                                                '<i class="fa fa-times"></i>'+
+                                        '</button>'+
+                            '</div>'+
+                            '<div class="panel-body">'+
+                                '<table id="detalle_'+nomTablero+'" class="table table-striped table-bordered table-condensed table-hover">'+
+                                    '<thead style="background-color:#A9D0F5">'+
+                                        '<th>Producto</th>'+
+                                        '<th>Cant.</th>'+
+                                        '<th>P. Unit.</th>'+
+                                        '<th>Importe</th>'+
+                                        //'<th></th>'+
+                                    '</thead>'+
+                                    '<tbody>'+
+                                    '</tbody>'+ 
+                                    // '<tfoot>'+
+                                    //     '<th>Total</th>'+
+                                    //     '<th></th>'+
+                                    //     '<th></th>'+
+                                    //     '<th><h4 id="total_'+nomTablero+'">s/. 0.00</h4><input type="hidden" name="precio_subtotal_'+nomTablero+'" id="precio_subtotal_'+nomTablero+'">'+
+                                    //     '</th>'+
+                                    // '</tfoot>'+
+                                '</table>'+
+                            '</div>'+
                         '</div>'+
-                        '<div class="panel-body">'+
-                            '<table id="detalle_'+nomTablero+'" class="table table-striped table-bordered table-condensed table-hover">'+
-                                '<thead style="background-color:#A9D0F5">'+
-                                    '<th>Producto</th>'+
-                                    '<th>Cant.</th>'+
-                                    '<th>P. Unit.</th>'+
-                                    '<th>Importe</th>'+
-                                '</thead>'+
-                                '<tbody>'+
-                                '</tbody>'+ 
-                                // '<tfoot>'+
-                                //     '<th>Total</th>'+
-                                //     '<th></th>'+
-                                //     '<th></th>'+
-                                //     '<th><h4 id="total_'+nomTablero+'">s/. 0.00</h4><input type="hidden" name="precio_subtotal_'+nomTablero+'" id="precio_subtotal_'+nomTablero+'">'+
-                                //     '</th>'+
-                                // '</tfoot>'+
+                        '<table class="table table-striped table-bordered table-condensed table-hover">'+
+                            '</tbody>'+ 
+                                '<tfoot>'+
+                                    '<th colspan="3" >Total</th>'+
+                                    '<th><h4 id="total">s/. 0.00</h4><input type="hidden" name="precio_subtotal" id="precio_subtotal"></th>'+
+                                '</tfoot>'+
                             '</table>'+
-                        '</div>'+
-                    '</div>'+
-                    '<table class="table table-striped table-bordered table-condensed table-hover">'+
-                        '</tbody>'+ 
-                            '<tfoot>'+
-                                '<th colspan="3" >Total</th>'+
-                                '<th><h4 id="total">s/. 0.00</h4><input type="hidden" name="precio_subtotal" id="precio_subtotal"></th>'+
-                            '</tfoot>'+
-                        '</table>'+
-                    '</table>';
-        }        
-    }
-    
-    $('#tablerosn').html(table);
-    if(tablero.length>0){
-        for (var i = 0; i < tablero.length; i++) {
-            for (var key in filaob) {
-                if (filaob.hasOwnProperty(key)) {
-                    if(tablero[i]==filaob[key]['nombre'])
-                    $('#detalle_'+tablero[i]).append(filaob[key]['fila']);
+                        '</table>';
+            } cont++;       
+        }
+        
+        $('#tablerosn').html(table);
+        if(tablero.length>0){
+            for (var i = 0; i < tablero.length; i++) {
+                for (var key in filaob) {
+                    if (filaob.hasOwnProperty(key)) {
+                        if(tablero[i]==filaob[key]['nombre'])
+                        $('#detalle_'+tablero[i]).append(filaob[key]['fila']);
+                    }
                 }
             }
         }
+        //nomtablero="";
     }
-    nomtablero="";
-}
 
-function elimiar(){
-    
-}
+    function eliminar(index){
+            //$("#"+nomtab).remove();
+            for (var key in filaob) {
+                    if (filaob.hasOwnProperty(key)) {
+                        if(index==filaob[key]['posi']){
+                            $("#fila_"+filaob[key]['nombre']+'_'+index).remove();
+                            filaob.splice(key,1);
+                            
+                    }
+                }
+            }  
+    }
+    function eliminarTablero(a){
+        for (const key in tablero) {
+            if (tablero.hasOwnProperty(key)) {
+                console.log(tablero);
 
-
-
+                console.log(tablero[key],a,key);
+                
+            }
+        }
+    }
 </script>
 @endpush
 @endsection
