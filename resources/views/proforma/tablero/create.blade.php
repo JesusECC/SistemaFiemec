beabf61a440bdf535b3c6a12264a6e35727d0f01
Jesus <jesuschilca@gmail.com>
2018-09-01 19:56:59 -0500

    tablero terminado

--- resources/views/proforma/tablero/create.blade.php
+++ resources/views/proforma/tablero/create.blade.php
@@ -1,16 +1,16 @@
 @extends ('layouts.admin')
 @section ('contenido')
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <h3>Nueva Proforma de tableros</h3>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<h3>Nueva Proforma de tableros</h3>
     <hr />
-    @if (count($errors)>0)
-    <div class="alert-alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach 
-        </ul>   
+	@if (count($errors)>0)
+	<div class="alert-alert-danger">
+		<ul>
+			@foreach ($errors->all() as $error)
+			    <li>{{$error}}</li>
+			@endforeach 
+		</ul>	
     </div>
     @endif
     <label class="control-label">Ingresar Nombre de tablero</label>
@@ -66,7 +66,7 @@
                         <input type="number" id="Pcantidad" class="form-control" name="Pcantidad" >
                     </div>
                 </div>
-                <div class="col-sm-1">
+                <div class="col-sm-2">
                     <div class="form-group label-floating">
                         <label class="control-label">Descuento %</label>
                         <input type="number" id="pdescuento" class="form-control" name="pdescuento" >
@@ -95,7 +95,7 @@
         <div id="tablerosn">
         
         </div>   
-        <div>
+        <div id="totales-general" style='display:none;'>
             <table class="table table-striped table-bordered table-condensed table-hover">
                     <tfoot>
                         <tr>
@@ -148,7 +148,9 @@
         });
         $('#bt_add_produc').click(function(){
             agregarProductosTablero();
-        });Pcantidad
+            document.getElementById('totales-general').style.display = 'block';
+
+        });
         $('#Pcantidad').keyup(function (){
             this.value = (this.value + '').replace(/[^0-9]/g, '');
         });
@@ -161,6 +163,9 @@
         $('#pdescuento').click(function (){
             this.value = (this.value + '').replace(/[^0-9]/g, '');
         });
+        // Actualizar
+       
+
     });
     var tablero=[];
     var filaob=[];
@@ -186,98 +191,15 @@
         // $("#producto-oculto").style.display='block';
     } 
 
-    function agregarProductosTablero(){    
-        var idProd=$('#idProd').val();
-        var pname=$("#Productoname ").val();
-        var puni=$('#precio_uni').val();
-        var pcant=$('#Pcantidad').val();
-        var sel=$('#prod-selec').val();
-        var descuento=$('#pdescuento').val();
-        console.log(descuento);
-        nomTablero=$('#prod-selec').val();
-        var filas;
-        // console.log(nomTablero,sel);
-        if(tablero.length>=0 && nomTablero!="" && idProd!="" && pname!="" && puni!="" && pcant!="" && nomTablero!="" && descuento!=""){
-            var bool=false;
-            var boolfila=false;
-            for (const key in tablero) {
-                if (tablero.hasOwnProperty(key)) {
-                    if(tablero[key]['nombre']==nomTablero){
-                        bool=true;
-                    }                    
-                }
-            }
-            console.log(filaob);
-            if(bool==true ){
-                for (const fil in filaob) {
-                    if (filaob.hasOwnProperty(fil)) {
-                        if(filaob[fil]['idProducto']==idProd){
-                            var su=parseInt(filaob[fil]['cantidadP'])+parseInt(pcant);
-                            filaob[fil]['cantidadP']=su;
-                            boolfila=true;
-                            alert("producto existente");
-                        }                    
-                    }
-                }
-                if(boolfila!=true){
-                    console.log("produc nuevoo")
-                    contp++;
-                    var dat={idProducto:idProd,producto:pname,prec_uniP:puni,cantidadP:pcant,descuentoP:descuento,nomTablero:nomTablero,posiP:contp};
-                    filaob.push(dat);
-                    subTotalTable();
-                    subTotal();
-                }
-                fila();
-            }
-            nomtablero="";
-        }
-    }
-    function fila(){
-        // realiza 
-        if(filaob.length>0){
-            var filas;
-            console.log(filaob.length+ " --> ");
-            for (const fila in filaob) {
-                if (filaob.hasOwnProperty(fila)) {
-                    var cantidad=parseFloat(filaob[fila]['cantidadP']);
-                    var precio=parseFloat(filaob[fila]['prec_uniP']);
-                     var subt=cantidad*precio;
-                    filas+=
-                        '<tr class="selected" id="fila_'+filaob[fila]['nomTablero']+'_'+filaob[fila]['posiP']+'">'+
-                            '<td> '+ 
-                                '<input type="hidden" name="idpod_'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['idProducto']+'">'+filaob[fila]['producto']+
-                            '</td>'+
-                            '<td> '+ 
-                                '<input type="number" disabled name="pcant'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['cantidadP']+'">'+
-                            '</td>'+
-                            '<td> '+   
-                                '<input type="number" disabled name="preuni'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['prec_uniP']+'">'+
-                            '</td>'+
-                            '<td> '+   
-                                '<input type="number" disabled name="pdescu'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['descuentoP']+'">'+
-                            '</td>'+
-                            '<td> '+   
-                                '<input type="number" disabled name="ptotal'+filaob[fila]['nomTablero']+'[]" value="'+subt +'">'+
-                            '</td>'+
-                            '<td>'+
-                                '<button type="button" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs" onclick="eliminar('+filaob[fila]['posiP']+');">'+
-                                        '<i class="fas fa-trash"></i>'+
-                                '</button>'+
-                            '</td>'+
-                        '</tr>';
-                        $('#detalle_'+nomTablero).html(filas);                    
-                }
-            }
-            // $('#detalle_'+nomTablero).append(filas);
-        }
-    }
+
     var bool;
     function agregarTablero(){    
         var tabl=$("#NomTablerop").val();
         nomTablero=tabl.replace(/ /gi,"_");  
         bool=false;  
+        // fila();
         if(tablero.length>=0 && nomTablero!=""){
-            //for para evitar tablas con el  mismo nombre execto las cuando inicia con mayusculas   
+            //for para evitar tablas con el  mismo nombre sin iportar las mayusculas o minisculas
             for (const key in tablero) {
                 if (tablero.hasOwnProperty(key)) {
                     if(tablero[key]['nombre'].toLowerCase()==nomTablero.toLowerCase()){
@@ -305,18 +227,16 @@
                                             '<div class="box-body">'+
                                                 '<div class="row">'+
                                                     '<div class="col-md-12">'+
-                                                        '<table  class="table table-striped table-bordered table-condensed table-hover">'+
+                                                        '<table id="detalle_'+nomTablero+'_Principal" class="table table-striped table-bordered table-condensed table-hover">'+
                                                             '<thead style="background-color:#A9D0F5">'+
-                                                                '<tr>'+
-                                                                    '<th>Producto</th>'+
-                                                                    '<th>Cant.</th>'+
-                                                                    '<th>P. Unit.</th>'+
-                                                                    '<th>Descuento</th>'+
-                                                                    '<th>Importe</th>'+
-                                                                '</tr>'+
+                                                                '<th>Producto</th>'+
+                                                                '<th>Cant.</th>'+
+                                                                '<th>P. Unit.</th>'+
+                                                                '<th>Descuento</th>'+
+                                                                '<th>Importe</th>'+
+                                                                //'<th></th>'+
                                                             '</thead>'+
-                                                            '<tbody id="detalle_'+nomTablero+'" >'+
-
+                                                            '<tbody id="detalle_'+nomTablero+'">'+
                                                             '</tbody>'+ 
                                                             '<tfoot>'+
                                                                 '<th>Total</th>'+
@@ -344,9 +264,102 @@
         // realiza el listado de todas los tableros que se añaden
         ListaSelect()
        // mantiene en la vista las filas cuando se agrega una nueva tabla
-        // detalleFilas();
-        fila();
+       detalleFilas();
+        // fila();
         //nomtablero="";
+    }
+    function agregarProductosTablero(){    
+        var idProd=$('#idProd').val();
+        var pname=$("#Productoname ").val();
+        var puni=$('#precio_uni').val();
+        var pcant=$('#Pcantidad').val();
+        var sel=$('#prod-selec').val();
+        var descuento=$('#pdescuento').val();
+        // console.log(descuento);
+        nomTablero=$('#prod-selec').val();
+        var filas;
+        // console.log(nomTablero,sel);
+        if(tablero.length>=0 && nomTablero!="" && idProd!="" && pname!="" && puni!="" && pcant!="" && nomTablero!="" && descuento!=""){
+            var bool=false;
+            var boolfila=false;
+            for (const key in tablero) {
+                if (tablero.hasOwnProperty(key)) {
+                    if(tablero[key]['nombre']==nomTablero){
+                        bool=true;
+                        for (const fil in filaob) {
+                            if (filaob.hasOwnProperty(fil)) {
+                                if(filaob[fil]['nomTablero']==nomTablero && filaob[fil]['idProducto']==idProd && filaob[fil]['nomTablero']==tablero[key]['nombre']){
+                                    var su=parseInt(filaob[fil]['cantidadP'])+parseInt(pcant);
+                                    filaob[fil]['cantidadP']=su;
+                                    fila();
+                                    boolfila=true;
+                                    console.log("Actualizar producto");                      
+                                }                
+                            }
+                        }
+                        if(boolfila==false){
+                            console.log("produc nuevoo",contp);
+                            var dat={idProducto:idProd,producto:pname,prec_uniP:puni,cantidadP:pcant,descuentoP:descuento,nomTablero:nomTablero,posiP:contp,fila:""};
+                            filaob.push(dat);
+                            fila();
+                            contp++;
+                            
+
+                        }
+                    }                    
+                }
+            }
+            detalleFilas();
+            subTotal();
+            console.log(filaob);            
+            nomtablero="";            
+        }
+    }
+    function fila(){
+        // realiza la insercion de las filas agregadas actualizando los importes y las cantidaddes
+        if(filaob.length>0){
+            var filas;
+            // console.log(filaob.length+ " --> ");
+            for (const key in tablero) {
+                if (tablero.hasOwnProperty(key)) {
+                    // $("#detalle_"+tablero[key]['nombre']).load();
+                    for (const fila in filaob) {
+                        if (filaob.hasOwnProperty(fila)) {                            
+                            var cantidad=parseFloat(filaob[fila]['cantidadP']);
+                            var precio=parseFloat(filaob[fila]['prec_uniP']);
+                            var subt=cantidad*precio;
+                            if(tablero[key]['nombre']==filaob[fila]['nomTablero']){
+                                filas=
+                                    '<tr class="selected" id="fila_'+filaob[fila]['nomTablero']+'_'+filaob[fila]['posiP']+'">'+
+                                        '<td> '+ 
+                                            '<input type="hidden" name="idpod_'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['idProducto']+'">'+filaob[fila]['producto']+
+                                        '</td>'+
+                                        '<td> '+ 
+                                            '<input type="number" disabled name="pcant'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['cantidadP']+'">'+
+                                        '</td>'+
+                                        '<td> '+   
+                                            '<input type="number" disabled name="preuni'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['prec_uniP']+'">'+
+                                        '</td>'+
+                                        '<td> '+   
+                                            '<input type="number" disabled name="pdescu'+filaob[fila]['nomTablero']+'[]" value="'+filaob[fila]['descuentoP']+'">'+
+                                        '</td>'+
+                                        '<td> '+   
+                                            '<input type="number" disabled name="ptotal'+filaob[fila]['nomTablero']+'[]" value="'+subt +'">'+
+                                        '</td>'+
+                                        '<td>'+
+                                            '<button type="button" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs" onclick="eliminar('+filaob[fila]['posiP']+');">'+
+                                                    '<i class="fas fa-trash"></i>'+
+                                            '</button>'+
+                                        '</td>'+
+                                    '</tr>';  
+                                    filaob[fila]['fila']=filas;
+                                    filas="";
+                            }                                                         
+                        }
+                    }                    
+                }
+            }
+        }
     }
     function ListaSelect(){
         // realiza el listado de todas los tableros que se añaden
@@ -368,20 +381,30 @@
             $('#tablerosn').html(tab);
         }
     }
-    // function detalleFilas(){
-    //     // mantiene en la vista las filas cuando se agrega una nueva tabla
-    //     if(tablero.length>0){
-    //         for (var keyt in tablero) {
-    //             for (var key in filaob) {
-    //                 if (filaob.hasOwnProperty(key)) {
-    //                     if(tablero[keyt]['nombre']==filaob[key]['nombre'])
-    //                     $('#detalle_'+tablero[keyt]['nombre']).append(filaob[key]['fila']);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     subTotalTable();
-    // }
+    function detalleFilas(){
+        // mantiene en la vista las filas cuando se agrega una nueva tabla
+        // var dat={idProducto:idProd,producto:pname,prec_uniP:puni,cantidadP:pcant,descuentoP:descuento,nomTablero:nomTablero,posiP:contp};
+        // fila(); 
+        var fil;
+        if(tablero.length>0){
+            for (var keyt in tablero) {
+                //  $("#detalle_"+tablero[keyt]['nombre']).load();
+                $("#detalle_"+tablero[keyt]['nombre']).val('');  
+                for (var key in filaob) {                   
+                    if (filaob.hasOwnProperty(key)) {
+                        if(tablero[keyt]['nombre']==filaob[key]['nomTablero']){
+                            fil+=filaob[key]['fila'];
+                        }
+                    }
+                }
+                $('#detalle_'+tablero[keyt]['nombre']).html(fil);
+                fil='';
+            }
+        }else{
+            $('#detalle_'+tablero[keyt]['nombre']).html('');
+        }
+        // subTotalTable();
+    }
     function subTotalTable(){
         // funcion para realizar la suma del sub total de todos los tableros que se declaran
         var sub=0;
@@ -390,8 +413,9 @@
                 if (tablero.hasOwnProperty(key)) {
                     for (const fila in filaob) {
                         if (filaob.hasOwnProperty(fila)) {
-                            if(tablero[key]['nombre']==filaob[fila]['nombre']){
-                                sub+=filaob[fila]['subtotal'];
+                            if(tablero[key]['nombre']==filaob[fila]['nomTablero']){
+                                sub+=parseFloat(filaob[fila]['prec_uniP'])*parseFloat(filaob[fila]['cantidadP']);
+                                console.log(sub,"---");
                             }                            
                         }
                     }   
@@ -403,10 +427,12 @@
         }
     }
     function subTotal(){
+        subTotalTable();
         var sub=0;
         for (const fila in filaob) {
             if (filaob.hasOwnProperty(fila)) {
-                sub+=filaob[fila]['subtotal'];                            
+                sub+=parseFloat(filaob[fila]['prec_uniP'])*parseFloat(filaob[fila]['cantidadP']);
+                console.log(sub);                        
             }
         }
 
@@ -415,15 +441,18 @@
     }
     function eliminar(index){
             // elimina las filas de un tablero especifico 
+            console.log(filaob,"eliminar",index);
             for (var key in filaob) {
                 if (filaob.hasOwnProperty(key)) {
                     if(index==filaob[key]['posiP']){
+                        console.log(filaob[key]['nomTablero'],"eliminar");
                         $("#fila_"+filaob[key]['nomTablero']+'_'+index).remove();
                         filaob.splice(key,1);
                         // console.log(filaob);                            
                     }
                 }
-            }  
+            } 
+            subTotal(); 
     }
     function eliminarTablero(a){
     // elimina todo un tablero con todos los datos que contiene
@@ -434,7 +463,7 @@
                     //console.log(tablero);
                     for (var k in filaob) {
                         if (filaob.hasOwnProperty(k)) {
-                            if(tablero[key]['nombre']==filaob[k]['nombre']){
+                            if(tablero[key]['nombre']==filaob[k]['nomTablero']){
                                 // console.log("encontrado");
                                 filaob.splice(k,1);
                             }
@@ -444,6 +473,13 @@
                     tablero.splice(key,1);                 
                 }              
             }
+        }
+        ListaSelect();
+    }
+    function ocultar(){
+        tablero.length>0
+        if (0<tablero.length && 0<filaob){
+            
         }
     }
 </script>
