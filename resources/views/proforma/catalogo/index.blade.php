8cca2ef934ec7f466cce2dfa74008b0b4a625288
jacrproyect <jcastillo@jacrproyectosweb.com>
2018-09-12 06:30:09 -0500

    producto - producto

--- resources/views/proforma/catalogo/index.blade.php
+++ resources/views/proforma/catalogo/index.blade.php
@@ -78,3 +78,36 @@
 -categoria ira en el detalle catalogo
 -estado ira en el detalle catalogo
  -->
+ <div class="row">
+  <div class="col-sm-12 col-md-12">
+  	@foreach($productos as $pro)
+	  <div class="col-sm-4 col-md-4">
+	    <div class="thumbnail">
+	      <img src="{{asset('fotos/productos/'.$pro->foto)}}">
+	      <div class="caption">
+	        <h3>{{$pro->nombre_producto}}</h3>
+	        <h4 style="color:#028039">S/. {{$pro->precio_unitario}} o </h4>
+	        <p style="font-size: 20px">
+	        	Stock: <span style="font">{{$pro->stock}}</span>
+	        </p>
+	        <p style="font-size: 20px">
+	        	<span>
+	        		Código <span style="font">{{$pro->codigo_producto}}</span>
+	        	</span>
+	        </p>
+	        <p>
+
+	        	<a  href=""  data-target="#modal-show-{{$pro->idProducto}}"  data-toggle="modal" class="btn btn-primary"><i class="far fa-eye"></i> Ver</a> 
+	        	
+	        	<a href="{{route('producto-edit',$pro->idProducto)}}" class="btn btn-success" role="button"><i class="fas fa-edit"></i> Editar</a> 
+	        	
+	        	<a href="" data-target="#modal-delete-{{$pro->idProducto}}"  data-toggle="modal" class="btn btn-danger" ><i class="fas fa-trash-alt"></i> Eliminar</a></p>
+	      </div>
+	    </div>
+	  </div>
+@include('proforma.producto.modal')
+	  				@include('proforma.producto.modal-producto')	  
+	 @endforeach
+  </div>
+  {{$productos->render()}}
+</div>
--- resources/views/proforma/producto/index.blade.php
+++ resources/views/proforma/producto/index.blade.php
@@ -7,37 +7,49 @@
 	@include('proforma.producto.search')
 </div>
 <div class="row">
-  <div class="col-sm-12 col-md-12">
-  	@foreach($productos as $pro)
-	  <div class="col-sm-4 col-md-4">
-	    <div class="thumbnail">
-	      <img src="{{asset('fotos/productos/'.$pro->foto)}}">
-	      <div class="caption">
-	        <h3>{{$pro->nombre_producto}}</h3>
-	        <h4 style="color:#028039">S/. {{$pro->precio_unitario}} o </h4>
-	        <p style="font-size: 20px">
-	        	Stock: <span style="font">{{$pro->stock}}</span>
-	        </p>
-	        <p style="font-size: 20px">
-	        	<span>
-	        		Código <span style="font">{{$pro->codigo_producto}}</span>
-	        	</span>
-	        </p>
-	        <p>
-
-	        	<a  href=""  data-target="#modal-show-{{$pro->idProducto}}"  data-toggle="modal" class="btn btn-primary"><i class="far fa-eye"></i> Ver</a> 
-	        	
-	        	<a href="{{route('producto-edit',$pro->idProducto)}}" class="btn btn-success" role="button"><i class="fas fa-edit"></i> Editar</a> 
-	        	
-	        	<a href="" data-target="#modal-delete-{{$pro->idProducto}}"  data-toggle="modal" class="btn btn-danger" ><i class="fas fa-trash-alt"></i> Eliminar</a></p>
-	      </div>
-	    </div>
-	  </div>
-	  @include('proforma.producto.modal')
-	  @include('proforma.producto.modal-producto')
-	 @endforeach
-  </div>
-  {{$productos->render()}}
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="table-responsive">
+			<table class=" table table-striped table-bordered table-condensed table-hover">
+				<thead>
+					<th align="center">Nº Serie</th>
+					<th align="center">Codigo</th>
+					<th align="center">Nombre</th>
+					<th align="center">Marca</th>		
+                    <th align="center">Precio</th>
+                    <th align="center">Opciones</th>
+				</thead>
+				<tbody>
+					@foreach($productos as $pro)
+					<tr>
+						<td>
+							{{$pro->serie_producto}}
+						</td>
+						<td>
+							{{$pro->codigo_producto}}
+						</td>
+						<td>
+							{{$pro->nombre_producto}}
+						</td>
+						<td>
+							{{$pro->marca_producto}}
+						</td>
+						<td>
+							S/. {{$pro->precio_unitario}}
+						</td>
+						<td align="center">
+							<a  href=""  data-target="#modal-show-{{$pro->idProducto}}"  data-toggle="modal" class="btn btn-primary"><i class="far fa-eye"></i> Ver</a>
+							<a href="{{route('producto-edit',$pro->idProducto)}}" class="btn btn-success" role="button"><i class="fas fa-edit"></i> Editar</a>
+							<a href="" data-target="#modal-delete-{{$pro->idProducto}}"  data-toggle="modal" class="btn btn-danger" ><i class="fas fa-trash-alt"></i> Eliminar</a>
+						</td>
+					</tr>
+					@include('proforma.producto.modal')
+	  				@include('proforma.producto.modal-producto')
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+		{{$productos->render()}}
+	</div>
 </div>
 @endsection
 
