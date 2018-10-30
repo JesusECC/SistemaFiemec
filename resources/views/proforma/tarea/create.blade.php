5dbdab690b28bc604ce2c0a91ed1c86973bd5e0e
jacrproyect <jcastillo@jacrproyectosweb.com>
2018-10-30 03:04:42 -0500

    Tarea Vista

--- resources/views/proforma/tarea/create.blade.php
+++ resources/views/proforma/tarea/create.blade.php
@@ -1,103 +1,119 @@
 @extends ('layouts.admin')
 @section ('contenido')
-<div class="row">
-	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-	<h3>Nuevo Ingreso</h3>
-	@if (count($errors)>0)
-	<div class="alert-alert-danger">
-		<ul>
-			@foreach ($errors->all() as $error)
-			    <li>{{$error}}</li>
-			@endforeach 
-		</ul>	
-    </div>
-    @endif
-    </div>
- </div>
+<section class="content-header">
+    <h1 style="margin-top: 55px;">
+        Panel de Administrador
+        <small>Version 2.3.0</small>
+    </h1>
+    <ol class="breadcrumb" style="margin-top: 55px;">
+        <li>
+            <a href="#">
+                <i class="fas fa-book"></i> Tareas</a>
+        </li>
+        <li class="active">Fiemec</li>
+        <li>
+            <a href="#">
+             Nuevo</a>
+        </li>
+    </ol>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box" style="border-top: 3px solid #18A689">
+                <div class="box-header with-border" style="padding: 10px !important">
+                    <h4>
+                        <strong style="font-weight: 400">
+                            <i class="fas fa-users"></i> Registro de Tarea
+                        </strong>
+                    </h4>
+                    @if(count($errors)>0)
+                    <div class="alert-alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach 
+                        </ul>   
+                    </div>
+                    @endif
 
-  {!!Form::open(array('url'=>'proforma/tarea','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
-
-
-
-<div class="box-body bg-gray-c">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="panel panel-default panel-shadow">
-                                <div class="panel-body">
-                                    <div class="form-group">
-                                        <label for="" class="control-label" style="color: #676a6c !important">
-                                            Tareas
-                                        </label>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6">
+                </div>
+            </div>
+            <!-- /.box-header -->
+            {!!Form::open(array('url'=>'proforma/tarea','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
+            <div class="box-body bg-gray-c">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="nav-tabs-custom">
+                            <div class="tab-content">
+                                <div class="active tab-pane" >
+                                    <div class="panel panel-default panel-shadow">
+                                        <div class="panel-body">
                                             <div class="form-group">
-                                                <input type="text" id="pnombre_tarea" class="form-control" placeholder="Ingrese nueva tarea">
-                                                    
-                                                
+                                                <label for="" class="control-label" style="color: #676a6c !important">
+                                                    Tareas
+                                                </label>
                                             </div>
-                                        </div>
-                                        <div class="col-sm-2">
-                                            <div class="form-group">
-                                                <input type="number" id="pprecioT" class="form-control" placeholder="Ingrese precio">
-                                                    
-                                                
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <div class="input-group">
+                                                        <input type="text" id="pnombre_tarea" class="form-control" placeholder="Ingrese nueva tarea">
+                                                        <samp class="input-group-btn">
+                                                            <button type="button" id="bt_add" class="btn btn-primary"><i class="fas fa-plus"></i> Agregar</button>
+                                                        </samp>
+                                                    </div>
+                                                </div>
                                             </div>
+                                            <div class="row" style="margin-top: 20px">
+                                                <div class="col-sm-12">
+                                                    <div class="form-group">
+                                                        <input type="number" id="pprecioT" class="form-control" placeholder="Ingrese precio">   
+                                                    </div>
+                                                </div>
+                                            </div>                                            
                                         </div>
-                                        <div class="col-sm-4">
-                                            <button type="button" id="bt_add" class="btn btn-primary"><i class="fas fa-plus"></i> Agregar</button>
-                                        </div>
-                                    
-                                    
-                                        
                                     </div>
                                 </div>
                             </div>
                         </div>
-                 </div>
-  
-             </div>
-            
-
-            <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-            	<table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
-            		<thead style="background-color:#A9D0F5">
-            			<th>opciones</th>
-            			<th>Tareas</th>
-                        <th>Precio</th>
-            			
-            		</thead>
-            		<tfoot>
-            			
-            			<th></th>
-            			<th></th>
-                        <th></th>
-            			
-            			
-
-            		</tfoot>
-            		
-            	</table>
-            	<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" id="guardar">
-       <div class="from-group">
-       	    <input name"_token" value="{{ csrf_token() }}" type="hidden"></input>
-	       <button class="btn btn-primary" type="submit">guardar</button>
-	       <button class="btn btn-danger" type="reset">cancelar</button>
-        </div>
-      </div>
-</div>
-            	
-      </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="nav-tabs-custom">
+                            <div class="tab-content">
+                                <div class="row">
+                                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                                        <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                                            <thead style="background-color:#A9D0F5">
+                                                <th>Tareas</th>
+                                                <th>Precio</th>
+                                                <th>Opciones</th>
+                                            </thead>
+                                            <tfoot>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                            </tfoot>
+                                        </table>
+                                    </div>                                            
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                </div><!-- /.row -->
             </div>
-      </div>
-</div>
-
-    
+            <div class="box-footer">
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" id="guardar">
+                    <div class="from-group">
+                        <input name"_token" value="{{ csrf_token() }}" type="hidden">
+                        <button class="btn btn-primary" type="submit">guardar</button>
+                        <button class="btn btn-danger" type="reset">cancelar</button>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+</section><!-- /.content -->
  {!!Form::close()!!}
-   
-
-
-
 
 @push('scripts')
 <script>
@@ -122,7 +138,7 @@ function agregar()
     if(tarea!="")
     {
        
-       var fila='<tr class="selected" id="fila'+cont+'"> <td><button type="button" class="btn btn-warning" onclick="eliminar('+cont+');">X</button></td> <td><input type="hidden" name="nombre_tarea[]" value="'+tarea+'">'+tarea+'</td> <td><input type="hidden" name="precioT[]" value="'+precio+'">'+precio+'</td></tr>';
+       var fila='<tr class="selected" id="fila'+cont+'"><td><input type="hidden" name="nombre_tarea[]" value="'+tarea+'">'+tarea+'</td> <td><input type="hidden" name="precioT[]" value="'+precio+'">'+precio+'</td><td><button type="button" class="btn btn-danger" onclick="eliminar('+cont+');">X</button></td></tr>';
        cont++;
        limpiar();
        evaluar();
